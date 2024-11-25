abcb471452f28379f157c7bdac48c7cdc0e69b84
Charles-Spaanheden <charliespaan@gmail.com>
2024-11-25 10:16:26 +0100

    Update

--- lib/request.rb
+++ lib/request.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
 class Request
+
+  def parammap(param)
+    param.map do |item|
+      item = item.split("=")
+      break if item[1] == nil
+      @params[item[0]] = item[1]
+    end
+  end
+
   def initialize(tolkningString)
     @headers = {}
     @params = {}
     @tolkningHash = tolkningString.split("\n")
     ingen_hash_line = @tolkningHash[0].split(' ')
-    method, resource, version = ingen_hash_line
-    def parammap(param)
-      param.map do |item|
-        item = item.split("=")
-        break if item[1] == nil
-        @params[item[0]] = item[1]
-      end
-    end
+    @method, @resource, @version = ingen_hash_line
+    @method = @method.downcase.to_sym
 
     if resource.include?("?")
       param = resource.split("?")
       param = param[1].split("&")
       parammap(param)
+      @resource = @resource.split("?")[0]
     end
 
     if @tolkningHash[-1].strip != ""
@@ -37,12 +41,12 @@ class Request
     
     
     
-    #puts @tolkningHash
-    puts method
-    puts resource
-    puts version
-    puts @headers
-    puts @params
+    # puts @tolkningHash
+    # puts @method
+    # puts @resource
+    # puts @version
+    # puts @headers
+    # puts @params
     
     
 
@@ -51,9 +55,10 @@ class Request
     #puts @tolkningHash
     #p @tolkningHash&.map {|grej| grej.end_with?(":")}
   end
+  attr_reader :method, :resource, :version, :headers, :params
 end
 
 #request_string = File.read('./test/example_requests/post-login.request.txt')
 #request_string = File.read('./test/example_requests/get-examples.request.txt')
-#request_string = File.read('./test/example_requests\get-fruits-with-filter.request.txt')
+request_string = File.read('./test/example_requests\get-fruits-with-filter.request.txt')
 Request.new(request_string)
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -3,19 +3,78 @@
 require_relative 'spec_helper'
 require_relative '../lib/request'
 
+# describe 'Request' do
+#   describe 'Simple get-request' do
+#     it 'parses the http method' do
+#       request_string = File.read('./test/example_requests/get-index.request.txt')
+#       request = Request.new(request_string)
+#       _(request.method).must_equal :get
+#     end
+
+#     it 'parses the resource' do
+#       request_string = File.read('./test/example_requests/get-index.request.txt')
+#       request = Request.new(request_string)
+#       _(request.resource).must_equal '/'
+#     end
+#   end
+# end
+
 describe 'Request' do
-  describe 'Simple get-request' do
-    it 'parses the http method' do
-      request_string = File.read('./test/example_requests/get-index.request.txt')
-      request = Request.new(request_string)
-      _(request.method).must_equal :get
+  describe 'Get-examples' do
+    before do
+      @request = Request.new(File.read("test/example_requests/get-examples.request.txt"))
+    end
+    
+    it 'method' do
+      assert_equal :get, @request.method
     end
 
-    it 'parses the resource' do
-      request_string = File.read('./test/example_requests/get-index.request.txt')
-      request = Request.new(request_string)
-      _(request.resource).must_equal '/'
+    it 'resource' do
+      assert_equal "/examples", @request.resource
     end
+
+    it 'version' do
+      assert_equal "HTTP/1.1", @request.version
+    end
+
+    it 'header' do
+      assert_equal ({"Host"=>"example.com", "User-Agent"=>"ExampleBrowser/1.0", "Accept-Encoding"=>"gzip, deflate", "Accept"=>"*/*"}), @request.headers
+    end
+
+    it 'param' do
+      assert_equal ({}), @request.params
+    end
+
   end
+
+  describe 'Get-fruits' do
+    before do
+      @request = Request.new(File.read("test/example_requests/get-fruits-with-filter.request.txt"))
+    end
+    
+    it 'method' do
+      assert_equal :get, @request.method
+    end
+
+    it 'resource' do
+      assert_equal "/fruits", @request.resource
+    end
+
+    it 'version' do
+      assert_equal "HTTP/1.1", @request.version
+    end
+
+    it 'header' do
+      assert_equal ({"Host"=>"fruits.com", "User-Agent"=>"ExampleBrowser/1.0", "Accept-Encoding"=>"gzip, deflate", "Accept"=>"*/*"}), @request.headers
+    end
+
+    it 'param' do
+      assert_equal ({"type"=>"bananas", "minrating"=>"4"}), @request.params
+    end
+
+  end
+      
 end
+
+
 
