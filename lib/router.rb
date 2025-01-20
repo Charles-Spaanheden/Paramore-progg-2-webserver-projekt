1808dfb72a11c24ccc6c8d8efed0243b99ad8d67
Charles-Spaanheden <charliespaan@gmail.com>
2025-01-20 10:12:16 +0100

    router upd p1

--- lib/router.rb
+++ lib/router.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
-
 class Router
-    
+    def initialize
+        @arr_of_routes = []
+    end
 
+    def add_route(type,name)
+     if type == :get || type == :post
+         route_arr = [type,name]
+         @arr_of_routes << route_arr
+        end
+    end
+
+    def match_route(source,browser)
+        browser_routes = [@method,@resource]
+        for routes in @arr_of_routes do
+            if routes == browser_routes
+                return 
+            end
+        end
+    end
 end
--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -1,6 +1,7 @@
 require 'socket'
 require_relative 'request'
 require_relative 'router'
+require_relative 'response'
 
 class HTTPServer
 
@@ -11,15 +12,19 @@ class HTTPServer
     def start
         server = TCPServer.new(@port)
         puts "Listening on #{@port}"
-        #router = Router.new
-        #router.add_route(:get, banan)
-        #router.add_route(:get, test1)
+        router = Router.new
+        router.add_route(:get,"/banan") do
+           "<h1>Hello, World banan?!</h1> <h2>bananbananbanan</h2>"
+        end
+        router.add_route(:get, "/test1") do
+            "<h1>Test</h1>"
+        end
 
         while session = server.accept
             data = ""
             while line = session.gets and line !~ /^\s*$/
                 data += line
-            endö
+            end
             puts "RECEIVED REQUEST"
             puts "-" * 40
             puts data
@@ -29,19 +34,28 @@ class HTTPServer
             
             pp request
 
-            if request.resource == "/"
+            # if request.resource == "/"
+            #     html = "<h1>Hello, World //!</h1>"
+            #     status = 200
+            # elsif request.resource == "/banan"
+            #     html = "<h1>Hello, World banan?!</h1> <h2>bananbananbanan</h2>"
+            # elsif request.resource == "/test1"
+            #     html = "<h1>Test</h1>"
+            # else
+            #     html = "<h1>Hello, World!</h1>"
+            #     status = 404          
+            # end
+
+            
+            route = router.match_route(@arr_of_routes,request)
+            
+            if route
                 html = "<h1>Hello, World //!</h1>"
                 status = 200
-            elsif request.resource == "/banan"
-                html = "<h1>Hello, World banan?!</h1> <h2>bananbananbanan</h2>"
-            elsif request.resource == "/test1"
-                html = "<h1>Test</h1>"
             else
-                html = "<h1>Hello, World!</h1>"
-                status = 404          
+                html = "<h1>Oh noes, World!</h1>"
+                status = 404
             end
-
-            #router.match_route(request)
             #Sen kolla om resursen (filen finns)
 
 
@@ -49,7 +63,7 @@ class HTTPServer
             # Nedanstående bör göras i er Response-klass
 
 
-            session.print "HTTP/1.1 200\r\n"
+            session.print "HTTP/1.1 #{status}\r\n"
             session.print "Content-Type: text/html\r\n"
             session.print "\r\n"
             session.print html
@@ -57,6 +71,5 @@ class HTTPServer
         end
     end
 end
-
 server = HTTPServer.new(4567)
 server.start
